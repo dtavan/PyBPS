6caa2221740ce2a2ff269fbb0fa47938ac3236b1
Damien Tavan <damien.tavan@aiguasol.coop>
2013-04-09 18:16:04 +0200

    Modified setup script. Now works as expected with pip install

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from distutils.core import setup
 
 setup(
     name='pybps',
-    version='0.1',
+    version='0.1.0',
     description='A parametric simulation manager for building performance simulation projects',
-    long_description=open('README.txt').read(),
+    long_description=open('README.rst').read(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Environment :: Console',
-        'Operating System :: Microsoft :: Windows :: Windows 7',
-        'Operating System :: Microsoft :: Windows :: Windows XP',
+        'Operating System :: Microsoft :: Windows',
         'License :: OSI Approved :: BSD License',
         'Intended Audience :: Science/Research',
         'Programming Language :: Python :: 2.7',
@@ -19,8 +18,9 @@ setup(
     url='http://github.com/aiguasol/pybps',
     author='Damien Tavan',
     author_email='damien.tavan@aiguasol.coop',
-    license='3-clause BSD',
-    packages=['pybps'],
+    license='BSD',
+    packages=['pybps','pybps.preprocess','pybps.postprocess'],
+    package_data={'pybps': ['config.ini']},
     install_requires=[
         "pandas",
     ],
